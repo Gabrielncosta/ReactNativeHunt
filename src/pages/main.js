5fdf2e214694100cd8844850902f78f89ae7642e
gabrielncosta <gabrielncosta208@gmail.com>
2019-12-18 08:47:14 -0300

    style

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text} from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class Main extends Component {
     static navigationOptions = {
@@ -24,14 +24,45 @@ export default class Main extends Component {
       this.setState({ docs });
     };
 
+    renderItem = ({ item }) => (
+      <View style={styles.productContainer}>
+        <Text style={styles.productTitle}>{item.title}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
+        <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+          <Text style={productButtonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
+    )
+
     render() {
         return(
-            <View>
-                <Text>Página Main: </Text>
-                {this.state.docs.map(product => (
-                <Text key={product._id}>{product.title}</Text>
-                ))}
-            </View>
+          <View style={styles.container}>
+            <FlatList contentContainerStyle={styles.list}
+            data={this.state.docs} keyExtractor={item => item._id} renderItem={this.renderItem}/>
+          </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa"
+  },
+
+  list: {
+    padding: 20,
+  },
+
+  productContainer: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 2,
+  },
+
+
+
+});
